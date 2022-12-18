973f2a65d76ad7ffda15da2867ae6549790aa2de
valera-orolin <92739665+valera-orolin@users.noreply.github.com>
2022-12-18 13:43:21 +0300

    Add files via upload

---
+++ DirectoryScaner.WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DirectoryScanner.Core;
+using DirectoryScanner.Core.Struct;
+using Ookii.Dialogs.Wpf;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DirectoryScaner.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<DirectoryNode> testNodes = new();
+        private CancellationTokenSource _token;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputFile_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dlg = new VistaFolderBrowserDialog();
+
+            if (dlg.ShowDialog() == true)
+            {
+                ((Button)sender).Tag = dlg.SelectedPath;
+
+                treeView1.ItemsSource = testNodes;
+
+                _token = new CancellationTokenSource();
+
+                testNodes.Clear();
+                Scanner scanner = new Scanner(4, dlg.SelectedPath, _token.Token);
+                testNodes.Add(scanner.Root);
+                Thread thread = new(scanner.StartProcess);
+                thread.Start();
+            }
+        }
+
+        private void CancelBTN_Click(object sender, RoutedEventArgs e)
+        {
+            _token.Cancel();
+        }
+    }
+}
